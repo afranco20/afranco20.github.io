474fa938986a21e0db1066d3ebe60d922aa046b5
Alex Franco <26696700+afranco20@users.noreply.github.com>
2019-11-05 01:46:54 -0500

    Updates

---
+++ precache-manifest.b76477c65a52afe8ec465c714a485dee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1c8b5d5cbb447582324814c6df0267c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1be3b7995410b33e14e1",
+    "url": "/static/css/main.720097aa.chunk.css"
+  },
+  {
+    "revision": "906ad5422891a1b9a6bd",
+    "url": "/static/js/2.af362d07.chunk.js"
+  },
+  {
+    "revision": "1be3b7995410b33e14e1",
+    "url": "/static/js/main.77b393b7.chunk.js"
+  },
+  {
+    "revision": "05129d9571d833ef1980",
+    "url": "/static/js/runtime-main.a5eaf332.js"
+  }
+]);
