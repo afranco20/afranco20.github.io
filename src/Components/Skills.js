6f903e7977ab9c803d225cb21f81522c0f7b45a5
Alex Franco <26696700+afranco20@users.noreply.github.com>
2019-11-07 11:25:05 -0500

    :lipstick: add icons for skills

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -1,14 +1,53 @@
-import React from 'react'
-import {Box, Typography, Paper, Container} from '@material-ui/core'
+import React from "react";
+
+// material ui
+import { Typography, Paper, Container } from "@material-ui/core";
+
+// font awesome
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faJava,
+  faPython,
+  faHtml5,
+  faCss3Alt,
+  faJs,
+  faBootstrap,
+  faReact,
+  faGitAlt,
+  faGithub,
+  faLinux
+} from "@fortawesome/free-brands-svg-icons";
 
 class Skills extends React.Component {
-  content() {
+  group(title, items) {
     return (
-      <Paper className="Skills">
-        <Typography variant="h4">
-          Skills
+      <React.Fragment>
+        <Typography variant="h4" component="h1">
+          {title}
         </Typography>
 
+        <Typography variant="h2" component="h2">
+          {items}
+        </Typography>
+      </React.Fragment>
+    );
+  }
+
+  content() {
+    const languages = [faJava, faPython, faHtml5, faCss3Alt, faJs];
+    const listItems1 = languages.map(x => (
+      <FontAwesomeIcon style={{ margin: "10px" }} key={x.iconName} icon={x} />
+    ));
+
+    const tools = [faLinux, faBootstrap, faReact, faGitAlt, faGithub];
+    const listItems2 = tools.map(x => (
+      <FontAwesomeIcon style={{ margin: "10px" }} key={x.iconName} icon={x} />
+    ));
+
+    return (
+      <Paper className="Skills">
+        {this.group("Programming languages", listItems1)}
+        {this.group("Tools", listItems2)}
       </Paper>
     );
   }
